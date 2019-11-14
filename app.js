67115f8c9a99f737abdfda24103a0eba81289ef2
Tim Kernell <tpkernell@gmail.com>
2019-11-14 14:33:17 -0600

    Basic 3Box, Web3, and Eth initialization in app.js

--- app.js
+++ app.js
@@ -8,7 +8,55 @@ var App = {
   spaceName: "myApp",
 
   init: function() {
+    return App.initWeb3();
+  },
 
+  initWeb3: function() {
+    if (typeof web3 !== "undefined") {
+      console.log("Using web3 detected from external source like Metamask");
+      App.web3Provider = web3.givenProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      console.log("Using localhost");
+      web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+    return App.initEth();
+  },
+
+  initEth: function() {
+    ethereum.enable().then(function() {
+      console.log("Ethereum enabled");
+      App.account = window.ethereum.selectedAddress;
+      console.log("In initEth: " + App.account);
+      return App.initBox();
+    });
+  },
+
+  initBox: function() {
+    console.log("Opening box...");
+    console.log("In initBox: " + App.account);
+    Box.openBox(App.account, window.ethereum).then(function(box) {
+      box.syncDone.then(function() {
+        console.log(box);
+        App.box = box;
+        return App.initSpace();
+      });
+    });
+  },
+
+  initSpace: function() {
+    console.log("Opening space...");
+    App.box.openSpace(App.spaceName).then(function(space) {
+      space.syncDone.then(function() {
+        console.log(space);
+        App.space = space;
+        return App.render();
+      });
+    });
+  },
+
+  render: function() {
+    
   }
 }
 
